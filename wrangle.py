74c2f0a2061c2d2ff85ac887feafec7816392d0d
Kwame Taylor <maxkwametaylor@gmail.com>
2021-03-01 19:48:21 -0600

    take verbose code oout of notebook in favor of functions in wrangle

--- wrangle.py
+++ wrangle.py
@@ -2,9 +2,20 @@ import pandas as pd
 import numpy as np
 
 def wrangle_insects():
+    # read from csv file
     insects = pd.read_csv('insects.csv')
+    # set the index to #
     insects = insects.set_index('#')
+    # drop columns I won't be using
     insects = insects.drop(columns=['Icon Filename', 'Critterpedia Filename', 'Furniture Filename', 'Internal ID', 'Unique Entry ID'])
+    # impute NaNs with 0s for now
     insects = insects.fillna(0)
+    # convert Spawn Rates to an integer and handle range value (brute force method for now)
+    # Because one of the values is a range, I'm going to make it the average of
+    # the start and end values rounded to the nearest integer, which is 8.
     insects['Spawn Rates'] = insects['Spawn Rates'].where(insects['Spawn Rates'] != '5–10', 8).astype(int)
+
+    assert insects.isna().sum().sum() == 0, "There are unhandled nulls in the DataFrame."
+    # will raise an AssertionError exception if there are any nulls that were unsuccessfully handled
+
     return insects
